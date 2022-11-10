6679c86f2c7abe6fa79d62ec21ca39d76cf41cb7
Palmieres Mota <palmierres22@gmail.com>
2022-11-10 20:26:37 -0300

    ajustando listagem de vagas

--- vagas/views.py
+++ vagas/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from empresa.models import Vagas
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def nova_vaga(request):
     if request.method == "POST":
         titulo = request.POST.get('titulo')
         email = request.POST.get('email')
-        tecnologias_domina = request.POST.get('tecnologias_domina')
-        tecnologias_nao_domina = request.POST.get('tecnologias_nao_domina')
+        tecnologias_domina = request.POST.getlist('tecnologias_domina')
+        tecnologias_nao_domina = request.POST.getlist('tecnologias_nao_domina')
         experiencia = request.POST.get('experiencia')
         data_final = request.POST.get('data_final')
         empresa = request.POST.get('empresa')
