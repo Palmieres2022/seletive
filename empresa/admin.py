dc05ec3d7fa8d4bd7402103e130b225fd6fe05f1
Palmieres Mota <palmierres22@gmail.com>
2022-11-14 00:16:56 -0300

    update

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Tecnologias, Empresa, Vagas
 
+
 admin.site.register(Tecnologias)
 admin.site.register(Empresa)
 admin.site.register(Vagas)
+
+
+
 
